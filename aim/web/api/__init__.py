28b0e2b1cf92ff456d6d9ed3798359af415c5cf6
Subramaniam Ramasubramanian <subramaniam.r20jan@gmail.com>
2023-02-08 13:31:47 +0000

    fix: add prefix only for static pages

--- aim/web/api/__init__.py
+++ aim/web/api/__init__.py
@@ -17,36 +17,45 @@ async def http_exception_handler(request, exc):
     detail = None
 
     if isinstance(exc.detail, dict):
-        message = exc.detail.pop('message', message)
-        detail = exc.detail.pop('detail', None)
+        message = exc.detail.pop("message", message)
+        detail = exc.detail.pop("detail", None)
 
-    response = {'message': message}
+    response = {"message": message}
     if detail:
-        response.update({'detail': detail})
+        response.update({"detail": detail})
     else:
-        response.update({'detail': str(exc)})
+        response.update({"detail": str(exc)})
     return JSONResponse(response, status_code=exc.status_code)
 
 
 async def fallback_exception_handler(request, exc):
-    response = {
-        'message': f'\'{type(exc)}\' exception raised!',
-        'detail': str(exc)
-    }
+    response = {"message": f"'{type(exc)}' exception raised!", "detail": str(exc)}
     return JSONResponse(response, status_code=500)
 
 
 def create_app():
-    app = FastAPI(title=__name__)
+    from aim.web.configs import AIM_UI_BASE_PATH
+
+    base_path = os.environ.get(AIM_UI_BASE_PATH, "")
+    base_path = f"/{base_path}/"
+    base_path = base_path.replace("//", "/")
+
+    app = FastAPI(title=__name__, root_path=base_path)
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=['*'],
-        allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-        allow_headers=['Origin', 'X-Requested-With',
-                       'Content-Type', 'Accept', 'Authorization', 'X-Timezone-Offset'],
+        allow_origins=["*"],
+        allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
+        allow_headers=[
+            "Origin",
+            "X-Requested-With",
+            "Content-Type",
+            "Accept",
+            "Authorization",
+            "X-Timezone-Offset",
+        ],
         allow_credentials=True,
-        max_age=86400
+        max_age=86400,
     )
 
     from aim.web.api.runs.views import runs_router
@@ -57,7 +66,6 @@ def create_app():
     from aim.web.api.dashboards.views import dashboards_router
     from aim.web.api.projects.views import projects_router
     from aim.web.api.views import statics_router
-    from aim.web.configs import AIM_UI_BASE_PATH
 
     from aim.web.api.projects.project import Project
     from aim.sdk.index_manager import RepoIndexManager
@@ -67,30 +75,30 @@ def create_app():
     index_mng = RepoIndexManager.get_index_manager(Project().repo)
     index_mng.start_indexing_thread()
 
-    api_app = FastAPI()
+    api_app = FastAPI(root_path=base_path)
     api_app.add_middleware(GZipMiddleware, compresslevel=1)
     api_app.add_exception_handler(HTTPException, http_exception_handler)
     api_app.add_exception_handler(Exception, fallback_exception_handler)
 
     if os.environ.get(AIM_PROFILER_KEY) == "1":
-        api_app.add_middleware(PyInstrumentProfilerMiddleware,
-                               repo_path=os.path.join(get_root_path(), get_aim_repo_name()))
+        api_app.add_middleware(
+            PyInstrumentProfilerMiddleware,
+            repo_path=os.path.join(get_root_path(), get_aim_repo_name()),
+        )
 
     add_api_routes()
 
-    api_app.include_router(dashboard_apps_router, prefix='/apps')
-    api_app.include_router(dashboards_router, prefix='/dashboards')
-    api_app.include_router(experiment_router, prefix='/experiments')
-    api_app.include_router(projects_router, prefix='/projects')
-    api_app.include_router(runs_router, prefix='/runs')
-    api_app.include_router(tags_router, prefix='/tags')
+    api_app.include_router(dashboard_apps_router, prefix="/apps")
+    api_app.include_router(dashboards_router, prefix="/dashboards")
+    api_app.include_router(experiment_router, prefix="/experiments")
+    api_app.include_router(projects_router, prefix="/projects")
+    api_app.include_router(runs_router, prefix="/runs")
+    api_app.include_router(tags_router, prefix="/tags")
 
-    base_path = os.environ.get(AIM_UI_BASE_PATH, '')
-
-    app.mount(f'{base_path}/api', api_app)
-    static_files_app = FastAPI()
+    app.mount("api", api_app)
+    static_files_app = FastAPI(root_path=base_path)
 
     static_files_app.include_router(statics_router)
-    app.mount(f'{base_path}/', static_files_app)
+    app.mount("/", static_files_app)
 
     return app
